918b0e20e6fa7e560c7e4c07861364d76ac7b4a0
Edmund Mills <edmund.martin.mills@gmail.com>
2026-05-30 14:13:59 -0500

    fix: wails build with webkit2_41 tags, fix ui command to find sibling binary

--- flake.nix
+++ flake.nix
@@ -10,6 +10,20 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        wailsDeps = with pkgs; [
+          pkg-config
+          gobject-introspection
+          gtk3
+          webkitgtk_4_1
+          libsoup_3
+          gsettings-desktop-schemas
+          glib
+        ];
+
+        wailsWrapped = pkgs.writeShellScriptBin "wails" ''
+          exec ${pkgs.wails}/bin/wails "$@" -tags webkit2_41
+        '';
       in
       {
         devShells.default = pkgs.mkShell {
@@ -20,11 +34,13 @@
             go-tools
             nodejs_22
             stdenv.cc.cc.lib
-          ];
+            wailsWrapped
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux wailsDeps;
 
           shellHook = ''
             export PATH="$PWD/bin:$PATH"
             export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+            export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}''${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}"
           '';
         };
       }
